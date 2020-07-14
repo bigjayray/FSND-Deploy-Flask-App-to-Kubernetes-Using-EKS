5c2bbe920bdb52fa6f5a848b885caf84f04e88e0
jeremiahidoko <jeremiahidoko@gmail.com>
2020-07-14 07:20:17 +0100

    updated requirements.txt

--- test_main.py
+++ test_main.py
@@ -31,9 +31,9 @@ def test_health(client):
 def test_auth(client):
     body = {'email': EMAIL,
             'password': PASSWORD}
-    response = client.post('/auth', 
-                           data=json.dumps(body),
-                           content_type='application/json')
+    response = client.post('/auth',
+                            data=json.dumps(body),
+                            content_type='application/json')
 
     assert response.status_code == 200
     token = response.json['token']
